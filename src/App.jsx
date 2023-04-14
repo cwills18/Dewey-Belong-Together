7dbd8719722395dffc87f2232bfe2c2270fd0e4d
cwills18 <102287175+cwills18@users.noreply.github.com>
2023-04-15 08:20:26 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 import "./index.scss";
 import React from "react";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes} from "react-router-dom";
 import FindBooksProvider from "./Contexts/FindBooksProvider";
 import SearchTermProvider from "./Contexts/SearchTermProvider";
 import HomePage from "./Containers/HomePage/HomePage";
@@ -13,16 +13,13 @@ function App() {
 		<FindBooksProvider>
 			<SearchTermProvider>
 				<div className="App">
-					<HomePage />	
-// 					<BrowserRouter>
-							
-// 							<Routes>
-// 								<Route exact path="/results" element={<ResultsPage />} />
-// 								<Route exact path="/:ISBN" element={<BookPage />} />
-// // 								//<Route path="/" element={} />
-// 							</Routes>
-		
-// 					</BrowserRouter>
+					<HashRouter>
+						<Routes>
+							<Route path="/" element={<HomePage />} />
+							<Route path="/results" element={<ResultsPage />} />
+							<Route path="/:ISBN" element={<BookPage />} />
+						</Routes>
+					</HashRouter>
 				</div>
 			</SearchTermProvider>
 		</FindBooksProvider>
